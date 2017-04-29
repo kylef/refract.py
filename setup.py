f163cb88be087074cfa0801bcb02d897e30f812a
Kyle Fuller <kyle@fuller.li>
2017-04-29 20:30:01 +0100

    fix(setup.py): Add refract.contrib to packages

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     version='0.1.0',
     description='A Python library for interacting with Refract.',
     url='https://github.com/kylef/refract.py',
-    packages=['refract', 'refract.elements'],
+    packages=['refract', 'refract.elements', 'refract.contrib'],
     author='Kyle Fuller',
     author_email='kyle@fuller.li',
     license='BSD',
